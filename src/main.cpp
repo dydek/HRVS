1ea39885be4ae99526da1409af29ef4b2b3f1570
dydek <michal.dydecki@10clouds.com>
2017-11-23 19:33:15 +0100

    simple chnges

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,29 @@
 //
 
 #include <Arduino.h>
+#include <Bounce2.h>
+#include <LiquidCrystal.h>
+#include <LiquidMenu.h>
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+LiquidLine welcome_line1(1, 0, "Hello Menu");
+
+LiquidLine welcome_line2(1, 1, "Hello test 2");
+
+LiquidScreen welcome_screen(welcome_line1, welcome_line2);
+
+LiquidMenu my_menu(lcd, welcome_screen);
 
 void setup() {
+    Serial.begin(9600);
+    Serial.println("Hello world!");
 
+    lcd.begin(16, 2);
+    my_menu.next_screen();
 }
 
 void loop() {
-    
 }
+
